17cddd25b84518ee508804d5c1af1f9355975e01
Maxwell Joslyn <maxwelljoslyn@gmail.com>
2024-04-13 22:21:37 -0700

    clean up code

--- gm_trainer/__init__.py
+++ gm_trainer/__init__.py
@@ -27,11 +27,7 @@ class Player:
         return f"{self.pc_name}: {response.text()}"
 
 
-alice = Player("Alice", "Arvak", 2, "fighter")
-bob = Player("Bob", "Bolzar", 3, "mage")
-
-
-players = [alice, bob]
+players = [Player("Alice", "Arvak", 2, "fighter"), Player("Bob", "Bolzar", 3, "mage")]
 
 
 def other_players_prompt(other_players):
@@ -39,52 +35,62 @@ def other_players_prompt(other_players):
 
 
 def system_prompt(p: Player):
-    other_players = [v for v in players if v.name != v.name]
+    other_players = [v for v in players if p.name != v.name]
     return dedent(
         f"""
-    You are {p.name}, a player participating in a roleplaying game session. Your character is {p.pc_name}, a level {p.pc_level} {p.pc_class}. Your fellow players include:
+    You are {p.name}, a player participating in a roleplaying game
+    session. Your character is {p.pc_name}, a level {p.pc_level}
+    {p.pc_class}. Your fellow players include:
     {other_players_prompt(other_players)}
 
-    The user is the Game Master (GM) of the session. The GM will describe a scenario to you, then ask you what you want to do. You can declare an action for your character, or ask questions of the GM until you're ready to declare an action. You must cooperate with your fellow players, acting as a team through your characters to accomplish shared goals.
+    The user is the Game Master (GM) of the session. The GM will
+    describe a scenario to you, then ask you what you want to do. You
+    can declare an action for your character, or ask questions of the
+    GM until you're ready to declare an action. You must cooperate
+    with your fellow players, acting as a team through your characters
+    to accomplish shared goals.
 
-    Respond with one declarative sentence or one question per prompt. No yapping. Do not describe more than one action, or give more than one question. Do not describe any game scenario elements, nor the actions of other characters."""
-    )
-
-
-scenario = f"""
-The year is 1651. You and your companions woke up dawn and traveled into the foothills of the mountains of Tenerife, the most important of the Canary Islands. Now you stand before a cave whose opening is as tall as two men and as wide as a wagon. You've been told that before these islands were conquered by the Spanish, the indigenous Guanches (who still exist) would bury their mummified dead in caverns like this."""
-
-
-def scenario_prompt(text: str):
-    return dedent(
-        f"""
-    GM: {text}
-
-    What do you do?"""
-    )
+    Respond with one declarative sentence or one question per prompt.
+    No yapping. Do not describe more than one action, or give more
+    than one question. Do not describe any game scenario elements, nor
+    the actions of other characters."""
+    ).strip()
 
 
-def player_turn(scenario):
-    print(scenario_prompt(scenario))
-    p1, p2 = players[0], players[1]
-    response = p1.conversation.prompt(
-        scenario_prompt(scenario), system=system_prompt(p1)
-    )
-    updated_scenario = "\n".join([scenario, p1.format_response(response)])
-    next_response = p2.conversation.prompt(
-        scenario_prompt(updated_scenario), system=system_prompt(p2)
-    )
-    final_scenario = "\n".join([updated_scenario, p2.format_response(next_response)])
-    print(p1.format_response(response))
-    print(p2.format_response(next_response))
-    return final_scenario
+def scenario_prompt(text):
+    return f"GM: {text}\n\nWhat do you do?"
 
 
-def game_loop():
-    first_turn = player_turn(scenario)
-    # TODO have to make sure that the player two response makes it to player one.
-    what_happens = input("What happens, GM?")
-    second_turn = player_turn(what_happens)
+def player_turn(scenario, initial=False):
+    if initial:
+        print(scenario_prompt(scenario))
+    for player in players:
+        response = player.conversation.prompt(
+            scenario_prompt(scenario), system=system_prompt(player)
+        )
+        scenario = f"{scenario}\n{player.format_response(response)}"
+        print(player.format_response(response))
+    return scenario
 
 
-game_loop()
+initial_scenario = dedent(
+    f"""
+The year is 1651. You and your companions woke up dawn and traveled
+into the foothills of the mountains of Tenerife, the most important of
+the Canary Islands. Now you stand before a cave whose opening is as
+tall as two men and as wide as a wagon. You've been told that before
+these islands were conquered by the Spanish, the indigenous Guanches
+(who still exist) would bury their mummified dead in caverns like
+this."""
+).strip()
+
+
+def game_loop(current_scenario):
+    current_scenario = player_turn(current_scenario, initial=True)
+    while True:
+        what_happens = input("GM: ")
+        # get all N players' most recent response to append...
+        current_scenario = player_turn(what_happens)
+
+
+game_loop(initial_scenario)
